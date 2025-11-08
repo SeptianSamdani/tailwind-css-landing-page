94eadd498c38530586cc649fece0b41e8d6e9a03
SeptianSamdany <227007072@student.unsil.ac.id>
2025-11-08 17:26:29 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Hero } from '@/components/Hero'
+
+export const App = () => {
+  return (
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
+      <Hero />
+    </main>
+  )
+}
